0b9b90a54a7b636c75c07bd6f440695710f9dc80
retajjjj <retagmohamed2004@gmail.com>
2025-04-28 17:45:55 +0300

    added files to run the expo project

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    presets: ['module:metro-react-native-babel-preset'],
-    plugins: ['react-native-reanimated/plugin'], // Add this line
-  };
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: ['react-native-reanimated/plugin', ["@babel/plugin-transform-private-methods", { "loose": true }]], // Add this line
+};
