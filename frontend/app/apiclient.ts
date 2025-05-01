9379b6a57e3cd54f39df804786686ee876a67e2e
xKimozZ <130156798+xKimozZ@users.noreply.github.com>
2025-05-01 21:16:05 +0300

    Embed token refresh to interceptor

--- frontend/app/apiclient.ts
+++ frontend/app/apiclient.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import * as SecureStore from 'expo-secure-store'; // Secure storage for tokens
+import { isTokenExpired, refreshAuthToken } from './utils/authHelpers'; // Token helpers
 
 const apiClient = axios.create({
   baseURL: 'http://quickbite.zapto.org', // Your local server
@@ -11,7 +12,20 @@ const apiClient = axios.create({
 // 🛡 Intercept all outgoing requests
 apiClient.interceptors.request.use(
   async (config) => {
-    const token = await SecureStore.getItemAsync('jwtToken'); // Get the stored token
+    let token = await SecureStore.getItemAsync('jwtToken');
+        if (!token) {
+          throw new Error('No token found');
+        }
+    
+        if (isTokenExpired(token)) {
+          console.log("Token expired, refreshing...");
+          token = await refreshAuthToken();
+          if (!token) {
+            throw new Error('Failed to refresh token');
+          }
+          await SecureStore.setItemAsync('jwtToken', token);
+        }
+    
     if (token) {
       config.headers.Authorization = `Bearer ${token}`; // Attach Authorization header
     }
