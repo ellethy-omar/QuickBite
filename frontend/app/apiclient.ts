a6aeff75752b199f6dbc5f300c156046f104f917
xKimozZ <130156798+xKimozZ@users.noreply.github.com>
2025-05-01 20:47:34 +0300

    Restored missed import

--- frontend/app/apiclient.ts
+++ frontend/app/apiclient.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import * as SecureStore from 'expo-secure-store'; // Secure storage for tokens
 
 const apiClient = axios.create({
   baseURL: 'http://quickbite.zapto.org', // Your local server
