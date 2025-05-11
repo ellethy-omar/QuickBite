9ddb831202e5086ccc0603b9b0b2cf10820468d1
basboosa05 <166862890+basboosa05@users.noreply.github.com>
2025-05-11 19:12:13 +0300

    Create Notification.js

---
+++ backend/models/Notification.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const NotificationSchema = new Schema({
+  senderId: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'Admin' // Only admins can send
+  },
+  receiverId: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    refPath: 'receiverModel' 
+  },
+  receiverModel: {
+    type: String,
+    required: true,
+    enum: ['User', 'Driver', 'Restaurant'] // Allowed receiver types
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  data: {
+    type: Schema.Types.Mixed, // Optional
+    default: null
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Notification', NotificationSchema);
